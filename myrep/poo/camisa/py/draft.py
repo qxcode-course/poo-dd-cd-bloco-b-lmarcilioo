a163a269c06f9137dde1c7021a76c3b1bb45b271
Lucas Marcilio <lucasmarcilio6@gmail.com>
2025-10-21 20:00:33 +0000

    FINALIZAÇÃO TAREFA RELOGIO E CORRECAO CHINELA.  1HR APROX.

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -3,22 +3,21 @@ class Tshirt:
         self.__size: str = ""
 
     def setSize (self, sizeAceppt: str):
-        if sizeAceppt != "PP" and sizeAceppt!= "P" and sizeAceppt!= "M" and sizeAceppt!= "G" and sizeAceppt!= "GG" and sizeAceppt!= "XG":
-           print ("Esse tamanho de camisa é inválido") 
-        else:
-            self.__size = sizeAceppt
-            return 
+        availableSizes = ["PP", "P", "M", "G", "GG", "XG"]
+        if sizeAceppt.upper() in availableSizes: 
+            self.__size = sizeAceppt.upper()
+            return
+        print ("Esse tamanho de camisa é inválido") 
+            
                    
     def getSize(self) -> str:
         return self.__size        
     
 def main():
     Camisa = Tshirt()
-    while True:
+    while Camisa.getSize() == "":
         print ("Informe o tamanho da roupa:")
         line = input()
         Camisa.setSize(line)
-
-        print("O tamanho da sua camisa é: "+ Camisa.getSize())
-        break
+    print("O tamanho da sua camisa é: "+ Camisa.getSize())
 main()
--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,9 +1,12 @@
+
 class Watch:
     def __init__(self, hora: int, min: int, seg: int):
-        self.__hora = hora
-        self.__min = min
-        self.__seg = seg
-
+        self.__hora = 0
+        self.__min = 0
+        self.__seg = 0
+        self.setHora(hora)
+        self.setMin(min)
+        self.setSeg(seg)
     def __str__(self) -> str:
         return f"{self.__hora:02}:{self.__min:02}:{self.__seg:02}"
 
@@ -15,10 +18,39 @@ class Watch:
 
     def getHora (self):
         return self.__hora
+    
+    def getMin (self):
+        return self.__min
+    
+    def getSeg (self):
+        return self.__seg
 
     def setMin (self, valor:int):
         if valor > 59 or valor < 0:
-            print()    
+            print("fail: minuto invalido") 
+            return
+        self.__min = valor
+
+    def setSeg (self, valor: int):
+        if valor > 59 or valor < 0:
+            print ("fail: segundo invalido")
+            return
+        self.__seg = valor   
+
+    def nextSecond (self):
+        if self.getSeg() != 59:
+            self.setSeg(self.getSeg()+1)
+
+        else:
+            self.setSeg(0) 
+            if self.getMin() != 59:
+                self.setMin(self.getMin()+1)
+            else: 
+                self.setMin(0)
+                if self.getHora() != 23:
+                    self.setHora (self.getHora()+1)
+                else:
+                    self.setHora(0)
 
 def main():
     relogio = Watch(0,0,0)
@@ -30,10 +62,18 @@ def main():
             break
         if args [0] == "show":
             print(relogio)
+        if args [0] == "init":
+            hora: int = int(args[1])
+            min: int = int(args [2])
+            seg: int = int(args [3])
+            relogio = Watch(hora, min, seg)
         if args [0] == "set":
             hora: int = int(args[1])
             min: int = int(args [2])
             seg: int = int(args [3])
-            relogio = Watch(hora,min,seg)
             relogio.setHora(hora)
+            relogio.setMin(min)
+            relogio.setSeg(seg)
+        if args [0] == "next":
+            relogio.nextSecond()
 main()
